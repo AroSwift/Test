4f6f49c69e210a1dbb8a7f3e9e6b047aeaeafc3d
AroSwift <abarlow505@gmail.com>
2016-04-18 17:36:07 -0400

    Move FANN source code to relative lib

--- lib/functions.h
+++ lib/functions.h
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <stdio.h>
 #include "data.h"
-#include "fann.h"
+// #include "fann.h"
+// // #include "./src/floatfann.c"
+#include "src/doublefann.c"
 
 void callNN(bool fileName);
 int FANN_API test_callback(struct fann *ann, struct fann_train_data *train,
