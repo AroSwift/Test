4b511d9c32773aa40fdd3de9a4cd99f93e123524
AroSwift <abarlow505@gmail.com>
2016-04-19 22:26:43 -0400

    Remove omp library hack

--- lib/process_website.rb
+++ lib/process_website.rb
@@ -50,19 +50,4 @@ class ProcessWebsite
     return false
   end
 
-  # def valid?(url)
-  #   url = URI.parse(url_string)
-  #   req = Net::HTTP.new(url.host, url.port)
-  #   req.use_ssl = (url.scheme == 'https')
-  #   path = url.path if url.path.present?
-  #   res = req.request_head(path || '/')
-  #   if res.kind_of?(Net::HTTPRedirection)
-  #     url_exist?(res['location']) # Go after any redirect and make sure you can access the redirected URL
-  #   else
-  #     res.code[0] != "4" #false if http code starts with 4 - error on your side.
-  #   end
-  # rescue Errno::ENOENT
-  #   false #false if can't find the server
-  # end
-
 end
